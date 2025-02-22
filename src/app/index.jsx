61cd071c343420180346742bc561d3e828b90258
sharif-jobayed <sharif.jobayed@gmail.com>
2025-02-22 07:15:44 +0600

    Set up NativeWind V4

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 import { Link } from 'expo-router';
+import '../global.css';
 
 const App = () => {
 	return (
 		<>
-			<View style={styles.container}>
-				<Text>Aora!</Text>
-				<Link href={"/profile"}  style={styles.link}>Go to profile</Link>
+			<View>
+				<Text className = "">Aora!</Text>
+				<Link href={"/profile"}>Go to profile</Link>
 			</View>
 		</>
 	);
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-	container: {
-		display: 'flex',
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	link: {
-		marginTop: 20,
-		color: 'blue',
-	}
-});
