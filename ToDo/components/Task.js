daa98d2c3067226ed9dd0da0d9faf7761587b24c
sharif-jobayed <sharif.jobayed@gmail.com>
2025-02-27 03:39:18 +0600

    Complete design

---
+++ ToDo/components/Task.js
@@ -0,0 +1,56 @@
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import {} from "react-native-web";
+
+const Task = (props) => {
+	return(
+		<>
+			<View style={styles.item}>
+
+				<View style={styles.itemLeft}>
+					<TouchableOpacity style={styles.square} activeOpacity={0.5}></TouchableOpacity>
+					<Text style={styles.itemText}>{props.text}</Text>
+				</View>
+
+				<View style={styles.circular}></View>
+
+			</View>
+		</>
+	);
+}
+
+export default Task;
+
+const styles = StyleSheet.create({
+	item:{
+		backgroundColor:'#FFF',
+		padding:15,
+		borderRadius:10,
+		flexDirection:'row',
+		alignItems:'center',
+		justifyContent:'space-between',
+		marginBottom:20,
+	},
+	itemLeft:{
+		flexDirection:'row',
+		alignItems:'center',
+		flexWrap:'wrap',
+	},
+	square:{
+		width:24,
+		height:24,
+		backgroundColor:'#55BCF6',
+		opacity:0.4,
+		borderRadius:5,
+		marginRight:15,		
+	},
+	itemText:{
+		maxWidth:'80%',
+	},
+	circular:{
+		width:12,
+		height:12,
+		borderColor:'#55BCF6',
+		borderWidth:2,
+		borderRadius:5,
+	},
+});
