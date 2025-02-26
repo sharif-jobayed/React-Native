6f35eb9a691ed537940b4959772b56fd264bc3b9
sharif-jobayed <sharif.jobayed@gmail.com>
2025-02-27 03:53:32 +0600

    Complete `Write a task`

--- ToDo/App.js
+++ ToDo/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import './global.css';
 import Task from './components/Task';
 
@@ -16,6 +16,15 @@ export default function App() {
           <Task text={`Task 3`}/>
         </View>
       </View>
+
+      <KeyboardAvoidingView style={styles.writeTaskWrapper} behavior={Platform.OS === `ios`? `padding`:`height`}>
+        <TextInput style={styles.input} placeholder={`Write a task`}/>
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -35,5 +44,35 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {
+    fontSize: 30,
   },
 });
